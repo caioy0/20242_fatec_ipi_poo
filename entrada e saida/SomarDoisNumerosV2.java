a686538a3e2d29a86ae827163814e027278ab2d9
caio <caio.yugo11@gmail.com>
2024-08-29 18:23:09 -0300

    chore: atualiza .gitignore

---
+++ entrada e saida/SomarDoisNumerosV2.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+public class SomarDoisNumerosV2{
+    static public void main(String args []){
+        //Declaracao de variaveis
+        double a, b, resultado;
+        //Entrada de dados
+        a = Double.parseDouble(JOptionPane.showInputDialog("Digite um valor"));
+        b = Double.parseDouble(JOptionPane.showInputDialog("Digite um outro valor"));
+        //Processamento
+        resultado = a + b;
+        //Saida
+        JOptionPane.showMessageDialog(
+            null, 
+            String.format("%.2f + %.2f = %.2f", a, b, resultado)
+        );
+    }
+}
