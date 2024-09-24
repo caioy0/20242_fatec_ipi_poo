58f9463879cd6ef3301d4bba8c45c7835ec8ff98
Caioy <caio.yugo11@gmail.com>
2024-09-24 14:02:31 -0300

    fix

--- jogo/Personagem.java
+++ jogo/Personagem.java
@@ -1,14 +1,28 @@
 public class Personagem{
+    //Variavel de instancia
     String nome;
     int energia = 10;
     int fome = 0;
     int sono = 0;
 
+    //Construtor padrao
+    Personagem(int energia, int fome, int sono){
+        //shadowing (sombreamento)
+        if (energia >= 0 && energia <= 10)
+            this.energia = energia;
+        if (fome >= 0 && fome <= 10)
+            this.fome = fome;
+        if (sono >= 0 && sono <= 10)
+            this.sono = sono;
+        System.out.println("Construindo um objeto...");
+    }
+
     void cacar(){
         if(energia >= 2){
             System.out.printf("%s caçando \n", nome);
             energia -= 2; // energia = energia -2
-        }else{
+        }
+        else{
             System.out.printf(
                 "%s sem energia para cacar\n",
                 nome
@@ -19,6 +33,7 @@ public class Personagem{
             //sono = sono +1 >10 ? sono : sono +1;
             //sono = sono == 1 ? sono : sono +1;
             sono = Math.min(sono +1, 10);
+            System.out.println(obterEstado());
         }
     }
         
@@ -27,8 +42,10 @@ public class Personagem{
             System.out.println(nome + " comendo");
             energia = Math.min (energia + 1, 10);
             fome--;
-        }else{
+        }
+        else{
             System.out.println(nome+" sem fome");
+            System.out.println(obterEstado());
         }
     }
     
@@ -37,14 +54,16 @@ public class Personagem{
             System.out.print(nome + " dormindo\n");
             energia = energia == 10 ? energia : energia + 1;
             sono -= 1;
-        }else{ 
+        }
+        else{ 
             System.out.print(nome + " sem sono\n");
+            System.out.println(obterEstado());
         }
     }
     public String toString(){
         //nome: e5, f4, s:8
         return String.format(
-            "%s: e%d, f%d, s%d",
+            "%s: e(%d), f(%d), s(%d)",
             nome, energia, fome, sono
         );
     }
