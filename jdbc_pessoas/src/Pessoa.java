764973d47d72c002d124057435dfaa0c226b3447
Caioy <caio.yugo11@gmail.com>
2024-11-21 19:22:23 -0300

    fix: crud

--- jdbc_pessoas/src/Pessoa.java
+++ jdbc_pessoas/src/Pessoa.java
@@ -1,11 +1,73 @@
-import lombok.*;
+// import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+// @Getter
+// @Setter
+// @AllArgsConstructor
+// @NoArgsConstructor
+
+// public class Pessoa {
+//     private int codigo;
+//     private String nome, fone, email;
+// }
 
 public class Pessoa {
-    private int codigo;
+    private int codigo = 0;
     private String nome, fone, email;
+  
+    public Pessoa(){
+  
+    }
+  
+    public Pessoa(int codigo){
+      this.codigo = codigo;
+    }
+  
+    public Pessoa(int codigo, String nome, String fone, String email){
+      this.codigo = codigo;
+      this.nome = nome;
+      this.fone = fone;
+      this.email = email;
+    }
+  
+    public int getCodigo() {
+      return codigo;
+    }
+  
+    public String getEmail() {
+      return email;
+    }
+  
+    public String getFone() {
+      return fone;
+    }
+  
+    public String getNome() {
+      return nome;
+    }
+  
+    public void setCodigo(int codigo) {
+      this.codigo = codigo;
+    }
+  
+    public void setEmail(String email) {
+      this.email = email;
+    }
+  
+    public void setFone(String fone) {
+      this.fone = fone;
+    }
+  
+    public void setNome(String nome) {
+      this.nome = nome;
+    }
+  
+    @Override
+    public String toString() {
+      return String.format(
+        "nome: %s, fone: %s, email: %s",
+        nome, fone, email
+      );
+    }
+  
+  
 }
